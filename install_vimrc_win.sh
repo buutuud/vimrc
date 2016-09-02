ea2c86132394aed60192fd72ecdffadd2d6824c2
happyyi <buutuud@gmail.com>
2016-09-02 17:03:12 +0800

    //更好的检查安装

--- install_vimrc_win.sh
+++ install_vimrc_win.sh
@@ -1,11 +1,26 @@
-cat .vimrc > ~/.vimrc
-echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
 if [[ -d $WINDIR ]]; then
-    echo "You VIM PlatForm Is Windows! Enjoy"
+	echo "You VIM PlatForm Is Windows! Enjoy"
+fi
+
+echo ""
+echo "1.Cat .Vimrc To ~/.vimrc"
+cat .vimrc > ~/.vimrc
+if [[ -d $WINDIR ]]; then
     if [[ -d ./plugins_w ]]; then
     	cp ./plugins_w/* $WINDIR
-    	ls ./plugins_w
-    	echo "Install ctags plugin to $WINDIR ..."
+    	plugins=`ls -C ./plugins_w | head`	#ls 单行输出
+    	echo "Install $plugins to $WINDIR ..."
     fi
 fi
 
+echo ""
+echo "2.Git Clone VBundle To .Vim Dir"
+VundlePlugin='~/.vim/bundle/Vundle.vim'
+if [ -d  $VundlePlguin]
+then
+	echo "VBundle Alredy Install ...."
+else
+	git clone https://github.com/VundleVim/Vundle.vim.git $VundlePlguin
+fi
+
+echo "OK..Installed the Ultimate Vim configuration successfully! Enjoy :-)"
